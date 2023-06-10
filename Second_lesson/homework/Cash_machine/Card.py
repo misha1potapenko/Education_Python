97da676e6dd9001a3a12557de42bdf0b4804d7b9
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-10 16:29:34 +0300

    added file for hex

---
+++ Second_lesson/homework/Cash_machine/Card.py
@@ -0,0 +1,26 @@
+class Card:
+
+    def __init__(self):
+        self.money = 0
+
+    def show_money(self):
+        """Show how money in card"""
+        print(f"In card: {self.money}")
+
+    def set_money(self, set_money):
+        """ Set balance in card"""
+        self.money = set_money
+        print(self.money)
+
+    def add_money(self, set_money):
+        """ Set balance in card"""
+        self.money += set_money
+        print(self.money)
+
+    def get_money(self, set_money):
+        """ Set balance in card"""
+        self.money -= set_money
+        print(self.money)
+
+
+
