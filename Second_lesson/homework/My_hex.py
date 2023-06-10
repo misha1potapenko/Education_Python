11002dd988a17acd6516815e788e32b08694f118
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-10 21:58:03 +0300

    added finish solution for task hex

--- Second_lesson/homework/My_hex.py
+++ Second_lesson/homework/My_hex.py
@@ -3,7 +3,31 @@
 
 my_number = int(input("Введите целое число "))
 
+
 def do_hex(my_number):
     list_for_hex = []
-    while True:
-    list_for_hex.append(my_number % 16 )
+    result = 0
+    string_for_list = ""
+    while my_number > 0:
+        result = my_number % 16
+        if result == 10:
+            list_for_hex.append("A")
+        elif result == 11:
+            list_for_hex.append("B")
+        elif result == 12:
+            list_for_hex.append("C")
+        elif result == 13:
+            list_for_hex.append("D")
+        elif result == 14:
+            list_for_hex.append("E")
+        elif result == 15:
+            list_for_hex.append("F")
+        list_for_hex.append(result)
+        my_number = my_number // 16
+        if my_number == 0:
+            list_for_hex.reverse()
+            print(''.join(str(el) for el in list_for_hex))
+
+
+do_hex(my_number)
+print(hex(my_number))
