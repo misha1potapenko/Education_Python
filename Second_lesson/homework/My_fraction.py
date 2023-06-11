ba6e091d9835eae665c9afa325fb0b527610521e
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-11 10:29:37 +0300

    added function sum

--- Second_lesson/homework/My_fraction.py
+++ Second_lesson/homework/My_fraction.py
@@ -4,7 +4,7 @@
 from fractions import Fraction
 
 
-def my_fractions ():
+def my_multiplication():
     fract1 = input("Input first fraction ")
     fract2 = input(("Input second fraction "))
     numerator1 = fract1.split("/")  # разобьем на числитель и знаменатель в список
@@ -33,3 +33,31 @@ def my_fractions ():
 
 
 my_fractions()
+
+def my_sum():
+    fract1 = input("Input first fraction ")
+    fract2 = input(("Input second fraction "))
+    numerator1 = fract1.split("/")  # разобьем на числитель и знаменатель в список
+    numerator2 = fract2.split("/")
+
+    if numerator1[1] == numerator2[1]:     # Если знаменатели равны
+        numerator = int(numerator1[0]) + int(numerator2[0])
+        denumerator = int(numerator1[1])
+
+    if (numerator % 2 == 0) and (denumerator % 2 == 0):
+        numerator = numerator/2
+        denumerator = denumerator/2
+    if (numerator % 2 == 0) and (denumerator % 2 == 0):
+        numerator = numerator/2
+        denumerator = denumerator/2
+    if (numerator % 3 == 0) and (denumerator % 3 == 0):
+        numerator = numerator/3
+        denumerator = denumerator/3
+    if (numerator % 7 == 0) and (denumerator % 7 == 0):
+        numerator = numerator/7
+        denumerator = denumerator/7
+
+    print(f"Умножение {int(numerator)}/{int(denumerator)}")
+
+    total = Fraction(fract1) + Fraction(fract2)
+    print(f"Проверка {total}")
