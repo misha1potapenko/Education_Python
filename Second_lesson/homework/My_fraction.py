8d2bbf444a6c160db5216fe8d5164d1d1f78084b
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-11 20:14:50 +0300

    added finish solution for fraction

--- Second_lesson/homework/My_fraction.py
+++ Second_lesson/homework/My_fraction.py
@@ -32,7 +32,10 @@ def my_multiplication():
     print(f"Проверка {total}")
 
 
-my_fractions()
+my_multiplication()
+
+
+
 
 def my_sum():
     fract1 = input("Input first fraction ")
@@ -44,6 +47,13 @@ def my_sum():
         numerator = int(numerator1[0]) + int(numerator2[0])
         denumerator = int(numerator1[1])
 
+    if numerator1[1] != numerator2[1]:     # Если знаменатели не равны
+        numerator = int(numerator1[0]) * int(numerator2[1]) + int(numerator2[0]) * int(numerator1[1])
+        denumerator = int(numerator1[1]) * int(numerator2[1])
+
+    if (numerator % 2 == 0) and (denumerator % 2 == 0):
+        numerator = numerator/2
+        denumerator = denumerator/2
     if (numerator % 2 == 0) and (denumerator % 2 == 0):
         numerator = numerator/2
         denumerator = denumerator/2
@@ -53,11 +63,31 @@ def my_sum():
     if (numerator % 3 == 0) and (denumerator % 3 == 0):
         numerator = numerator/3
         denumerator = denumerator/3
+    if (numerator % 3 == 0) and (denumerator % 3 == 0):
+        numerator = numerator/3
+        denumerator = denumerator/3
+    if (numerator % 3 == 0) and (denumerator % 3 == 0):
+        numerator = numerator/3
+        denumerator = denumerator/3
+    if (numerator % 7 == 0) and (denumerator % 7 == 0):
+        numerator = numerator/7
+        denumerator = denumerator/7
+    if (numerator % 7 == 0) and (denumerator % 7 == 0):
+        numerator = numerator/7
+        denumerator = denumerator/7
     if (numerator % 7 == 0) and (denumerator % 7 == 0):
         numerator = numerator/7
         denumerator = denumerator/7
 
-    print(f"Умножение {int(numerator)}/{int(denumerator)}")
+
+    if numerator == denumerator:
+        print("Сумма равна 1")
+    elif denumerator == 1:
+        print(f"Сумма = {numerator}")
+    else:
+        print(f"Сумма = {int(numerator)}/{int(denumerator)}")
 
     total = Fraction(fract1) + Fraction(fract2)
     print(f"Проверка {total}")
+
+my_sum()
