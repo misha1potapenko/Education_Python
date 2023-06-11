70e0533b8118afb5941ae70be812f62f649dd409
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-11 10:16:49 +0300

    added multiplication

--- Second_lesson/homework/My_fraction.py
+++ Second_lesson/homework/My_fraction.py
@@ -2,7 +2,34 @@
 # дробь с числителем и знаменателем. Программа должна возвращать сумму и
 # произведение* дробей. Для проверки своего кода используйте модуль fractions.
 from fractions import Fraction
-fract1 = input("Input first fraction ")
-fract2 = input(("Input second fraction "))
-total = Fraction(fract1) * Fraction(fract2)
-print(total)
+
+
+def my_fractions ():
+    fract1 = input("Input first fraction ")
+    fract2 = input(("Input second fraction "))
+    numerator1 = fract1.split("/")  # разобьем на числитель и знаменатель в список
+    numerator2 = fract2.split("/")
+
+    numerator = int(numerator1[0]) * int(numerator2[0])
+    denumerator = int(numerator1[1]) * int(numerator2[1])
+
+    if (numerator % 2 == 0) and (denumerator % 2 == 0):
+        numerator = numerator/2
+        denumerator = denumerator/2
+    if (numerator % 2 == 0) and (denumerator % 2 == 0):
+        numerator = numerator/2
+        denumerator = denumerator/2
+    if (numerator % 3 == 0) and (denumerator % 3 == 0):
+        numerator = numerator/3
+        denumerator = denumerator/3
+    if (numerator % 7 == 0) and (denumerator % 7 == 0):
+        numerator = numerator/7
+        denumerator = denumerator/7
+
+    print(f"Умножение {int(numerator)}/{int(denumerator)}")
+
+    total = Fraction(fract1) * Fraction(fract2)
+    print(f"Проверка {total}")
+
+
+my_fractions()
