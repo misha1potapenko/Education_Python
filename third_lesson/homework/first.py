2e8ecb06f95cc7fbf651f1f68b72c820214a5cfe
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-12 21:36:35 +0300

    added finish solution for task 1

--- third_lesson/homework/first.py
+++ third_lesson/homework/first.py
@@ -6,9 +6,12 @@ my_list = [3, 5, 7, 11, 5, 7, 77, 88, 99]
 result_list = []
 step = 0
 for i in my_list:
+    step = 0
     for j in my_list:
         if i == j:
             step += 1
         if step == 2:
-            result_list.append(i)
+            if i not in result_list:
+                result_list.append(i)
+                step = 0
 print(result_list)
