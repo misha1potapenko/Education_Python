c56305ca0c690aa6ff9fb6bf9fc43eb8f34cb552
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-12 21:28:00 +0300

    added new task for list sort

--- third_lesson/homework/first.py
+++ third_lesson/homework/first.py
@@ -1,3 +1,14 @@
 # ✔ Дан список повторяющихся элементов. Вернуть список
 # с дублирующимися элементами. В результирующем списке
 # не должно быть дубликатов.
+
+my_list = [3, 5, 7, 11, 5, 7, 77, 88, 99]
+result_list = []
+step = 0
+for i in my_list:
+    for j in my_list:
+        if i == j:
+            step += 1
+        if step == 2:
+            result_list.append(i)
+print(result_list)
