b3b856b094f7932c85766828ffbaf4a523084734
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-14 19:03:56 +0300

    added new file for task 3

--- third_lesson/homework/second.py
+++ third_lesson/homework/second.py
@@ -55,18 +55,18 @@ count_duplicate(text)
 # Ниже код для подсчета элементов и внесения в словарь
 
 
-# def count_duplicates(lst):
-#     counts = {}  # Создаем пустой словарь для подсчета повторений
-#     for item in lst:
-#         if item in counts:
-#             counts[item] += 1  # Увеличиваем значение для существующего элемента
-#         else:
-#             counts[item] = 1  # Добавляем новый элемент в словарь с начальным значением 1
-#
-#     return counts
-#
-#
-# duplicates = count_duplicates(my_list)
-# for item, count in duplicates.items():
-#     if count > 1:
-#         print(f"Элемент {item} повторяется {count} раз(а).")
+def count_duplicates(lst):
+    counts = {}  # Создаем пустой словарь для подсчета повторений
+    for item in lst:
+        if item in counts:
+            counts[item] += 1  # Увеличиваем значение для существующего элемента
+        else:
+            counts[item] = 1  # Добавляем новый элемент в словарь с начальным значением 1
+
+    return counts
+
+
+duplicates = count_duplicates(my_list)
+for item, count in duplicates.items():
+    if count > 4:
+        print(f"Элемент {item} повторяется {count} раз(а).")
