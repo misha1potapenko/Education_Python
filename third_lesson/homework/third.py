c9369d522a89c08b4f7007064fcd09a9e9d2776b
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-14 19:18:05 +0300

    added finish solution for third task

--- third_lesson/homework/third.py
+++ third_lesson/homework/third.py
@@ -4,8 +4,19 @@
 # грузоподъёмность. Достаточно вернуть один допустимый вариант.
 
 my_dict = {'котелок': 0.4,
-           'рюкзак': 1.2,
+           'вода': 1.2,
            'палатка': 3.2,
            'ложка': 0.2,
            'ножик': 0.3,
            'фонарик': 0.7, }
+
+
+def backpack_capacity(my_dict: dict, load_capacity):
+    load = 0
+    for y, x in my_dict.items():
+        load += x
+        if load < load_capacity:
+            print(y)
+
+
+backpack_capacity(my_dict, 2)
