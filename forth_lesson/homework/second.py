2da3208b7808e0221c5d814345a32106bf849c82
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-17 22:24:15 +0300

    added second task

--- forth_lesson/homework/second.py
+++ forth_lesson/homework/second.py
@@ -2,3 +2,12 @@
 # параметры и возвращающую словарь, где ключ — значение
 # переданного аргумента, а значение — имя аргумента. Если
 # ключ не хешируем, используйте его строковое представление.
+
+
+def my_kwargs(owner, **pets):
+    print(f"Owner Name: {owner}")
+    for pet, name in pets.items():
+        print(f"{pet}: {name}")
+
+my_kwargs("Jonathan", dog="Brock", fish=["Larry", "Curly", "Moe"], turtle="Shelldon")
+
