8e0d37e0ccd81695ac5e7da4c470a594e833f3cc
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-18 16:10:05 +0300

    added function fibonachu

--- fifth_seminar/homework/third.py
+++ fifth_seminar/homework/third.py
@@ -1,1 +1,13 @@
 # ✔Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+
+
+def fibonachi(num):
+    fib = []
+    for i in range(num):
+        if i == 1:
+            fib.append(i)
+        if i == 2:
+            fib.append(i)
+        else:
+            fib.append(fib[i-1] + fib[i-2] )
