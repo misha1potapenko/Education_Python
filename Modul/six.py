3d2cfbfddab11e13e630c48e934b30155b842003
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-19 20:47:35 +0300

    added new task

--- Modul/six.py
+++ Modul/six.py
@@ -3,14 +3,37 @@
 # Внутри генерируется случайное число в указанных границах и пользователь должен угадать его за заданное число попыток.
 # Функция выводит подсказки “больше” и “меньше”.
 # Если число угадано, возвращается истина, а если попытки исчерпаны - ложь.
-
-#
-# def predict(top_livel: int, down_lewel: int, count_level: int ):
-#     my_random = random.
+import random
 
 
-# Вспомните какие модули вы уже проходили на курсе.
-# Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта).
+def guess_f(minimum, maximum, tries):
+    number = random_number = random.randint(minimum, maximum)
+
+    while tries:
+        answer = int(input("Какое число загадано? "))
+
+        if answer == number:
+            print("Вы угадали!")
+            break
+        else:
+            if answer > number:
+                print("Попробуйте взять меньше")
+
+            else:
+                print("Попробуйте взять больше")
+        tries -= 1
+
+
+
+    else:
+        print("Вы не угадали(")
+        print("Было загадано ", number)
+
+
+if __name__ == "__main__":
+    guess_f(1, 10, 3)
+
+
 
 
 
