1eb7cedfa7d549582dd5e7ca5a7bcbc6932e8ef8
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-19 20:17:12 +0300

    added file for module

---
+++ Modul/new_file.py
@@ -0,0 +1,16 @@
+# Вспомните какие модули вы уже проходили на курсе.
+# Создайте файл, в котором вы импортируете встроенные в модуль функции под псевдонимами. (3-7 строк импорта).
+
+
+
+import random as rn
+import sys as s
+import math as mth
+
+my_number = rn.randint(1, 10)
+print(my_number)
+
+a_cos = mth.acos(0.5)
+print(a_cos)
+
+s.exit()
