b36258c08129c59f3ea7b25fa6ff38e6947a9b29
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-27 19:46:50 +0300

    added new directory for os

--- file_file_sistem/lecture.py
+++ file_file_sistem/lecture.py
@@ -6,3 +6,5 @@ with open('new_data.txt', 'a', encoding='utf-8') as f:
     for line in text:
         res = f.write(f'{line}\n')
 print(f'{res = }\n{len(line) = }')
+
+
---
+++ file_file_sistem/work_with_file_directory/first.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+print(os.getcwd())
+print(Path.cwd())
+os.chdir('../..')
+print(os.getcwd())
+print(Path.cwd())
