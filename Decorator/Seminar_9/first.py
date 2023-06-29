99d1b999ad6099b85033ed5faa3995c25eaf1c73
misha1potapenko <misha1potapenko@yandex.ru>
2023-06-29 19:31:55 +0300

    added task1 for file system

---
+++ Decorator/Seminar_9/first.py
@@ -0,0 +1,8 @@
+import os
+
+files = os.listdir(os.path.join(os.getcwd()))
+
+
+print(os.path)
+print(os.listdir())
+print(os.getcwd())
--- file_file_sistem/work_with_file_directory/task_1.py
+++ file_file_sistem/work_with_file_directory/task_1.py
@@ -3,6 +3,16 @@
 # ✔ Первое число int, второе - float разделены вертикальной чертой.
 # ✔ Минимальное число - -1000, максимальное - +1000.
 # ✔ Количество строк и имя файла передаются как аргументы функции.
+import random
 
-def fills_in_the_file(file_name:str, count_string:int):
+
+def fills_in_the_file(file_name: str, count_string: int):
     with open(file_name, 'a', encoding='utf-8') as f:
+        for i in range(count_string):
+            first_number = random.randint(-1000, 1000)
+            second_number = random.uniform(-1000, 1000)
+            result = (str(first_number) + '|' + str(second_number) + '\n')
+            file = f.write(result)
+
+
+fills_in_the_file('file.txt', 15)
