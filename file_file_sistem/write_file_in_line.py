a6bdc92afeb66b8a00820b00e0683922299d3ef5
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-01 15:27:28 +0300

    added working with file

---
+++ file_file_sistem/write_file_in_line.py
@@ -0,0 +1,20 @@
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+             'Consequatur debitis explicabo laboriosam sint suscipit temporibus veniam?',
+                          'Accusantium alias amet fugit iste neque non odit quiasaepetotamvelit?', ]
+
+text_macarenko = ['В то же время Макаренко хорошо понимал украинский язык (хотя, как отмечал тот же Хиллиг,',
+                 ' испытывал определённые трудности, когда Колонию обязали одно время представлять письменные',
+                 ' отчёты только на украинском языке) и умело включал отдельные сочные украинизмы в тексты своих',
+                 ' произведений. Как свидетельствует один из воспитанников Макаренко по коммуне имени Дзержинского,',
+                 ' поляк по происхождению, Леонид Конисевич в своей книге «Нас воспитал Макаренко»,' ,
+                 ' Антон Семёнович понимал многие фразы и мог кратко общаться и на польском языке, ',
+                 'что выяснилось при приезде польской делегации[8].']
+
+
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    f.writelines('\n'.join(text_macarenko))
+    for line in text:
+        res = f.write(f'{line}\n')
+print(f'{res = }\n{len(line) = }')
+
+
