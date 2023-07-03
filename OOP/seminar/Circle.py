f607f9f4cc05b6fe0238f6324aabfd01abf0ba79
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-03 16:35:15 +0300

    added Circle

--- OOP/seminar/Circle.py
+++ OOP/seminar/Circle.py
@@ -3,3 +3,23 @@
 # экземпляра.
 # У класса должно быть два метода, возвращающие длину
 # окружности и её площадь.
+import math
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def circumference_length(self):
+        # print((2*math.pi*self.radius)**2)
+        return 2*math.pi*self.radius
+
+    def square(self):
+        return (math.pi * self.radius)**2
+
+
+
+
+circle_1 = Circle(40)
+print(circle_1.circumference_length())
+print(circle_1.square())
