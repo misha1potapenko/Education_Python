487ee71dc616267c6d86d83058aebc1d424829f8
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-04 17:25:43 +0300

    added JSON append

--- JSON/homework/json_csv_pickle.py
+++ JSON/homework/json_csv_pickle.py
@@ -8,12 +8,14 @@
 # # размер файлов в ней с учётом всех вложенных файлов и директорий.
 import json
 import os
+import csv
 from os.path import join, getsize
 
 
 def walk_in_directory(file_path):
     for root, dirs, files in os.walk(file_path):
         folder = root.split('\\')
+        # это просмотр кто, что выдает
         print(f'В директории {folder[-1]}', end="\n")
         if not dirs:
             print("В этой папке нет директорий")
@@ -21,42 +23,18 @@ def walk_in_directory(file_path):
 
             print(f'Директория(и) {dirs}', end="\n")
         print(f'Файл(ы) {files}', end="\n")
-        # print(f' В директории {root}', end="\n")
-
         print(sum(getsize(join(root, name)) for name in files), end="\n")
+        print(root)
+        # запись в JSON file
+        str_for_json = " ".join(files)
+        to_json = {root: dirs, 'files': str_for_json, 'Size': sum(getsize(join(root, name)) for name in files) }
+        with open('to_json.json', 'a') as f:
+            json.dump(to_json, f, sort_keys=True, indent=2)
 
-        to_json = {'trunk': trunk_template, 'access': access_template}
-
-        with open('sw_templates.json', 'w') as f:
-            json.dump(to_json, f)
 
 
-        # print("bytes in", len(files), "non-directory files")
-        # if 'CVS' in dirs:
-        #     # не просматриваем каталог `CVS`
-        #     dirs.remove('CVS')
 
 
 walk_in_directory('C:\\Users\\misha\\PycharmProjects\\Education_Python\\JSON')
 
 
-import json
-
-trunk_template = [
-    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
-    'switchport trunk native vlan 999', 'switchport trunk allowed vlan'
-]
-
-access_template = [
-    'switchport mode access', 'switchport access vlan',
-    'switchport nonegotiate', 'spanning-tree portfast',
-    'spanning-tree bpduguard enable'
-]
-
-to_json = {'trunk': trunk_template, 'access': access_template}
-
-with open('sw_templates.json', 'w') as f:
-    json.dump(to_json, f)
-
-with open('sw_templates.json') as f:
-    print(f.read())
