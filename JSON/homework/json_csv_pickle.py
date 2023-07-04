926178e9e86a3c3bfc07e55edcfe4b4370d2d723
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-04 18:12:09 +0300

    added CSV append

--- JSON/homework/json_csv_pickle.py
+++ JSON/homework/json_csv_pickle.py
@@ -9,6 +9,7 @@
 import json
 import os
 import csv
+import pickle
 from os.path import join, getsize
 
 
@@ -25,11 +26,24 @@ def walk_in_directory(file_path):
         print(f'Файл(ы) {files}', end="\n")
         print(sum(getsize(join(root, name)) for name in files), end="\n")
         print(root)
+
         # запись в JSON file
         str_for_json = " ".join(files)
         to_json = {root: dirs, 'files': str_for_json, 'Size': sum(getsize(join(root, name)) for name in files) }
         with open('to_json.json', 'a') as f:
             json.dump(to_json, f, sort_keys=True, indent=2)
+
+        # запись в CSV  file
+        with open('for_csv.csv', 'a', newline='') as csvfile:
+            fieldnames = ['files', 'dirs']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({'files': str_for_json, 'dirs': dirs})
+
+        # запись в pickle  file
+        with open('data.pickle', 'wb') as f:
+            pickle.dump(to_json, f)
 
 
 
