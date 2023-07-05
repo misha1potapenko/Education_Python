0c942711493e58cceef7c2f7daafca514ccdcce6
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-05 18:10:19 +0300

    added new function csv file

--- Decorator/homework/task_1.py
+++ Decorator/homework/task_1.py
@@ -8,3 +8,35 @@
 # ○ Декоратор, сохраняющий переданные параметры и результаты работы
 # функции в json файл.
 # Соберите пакет с играми из тех файлов, что уже были созданы в рамках курса
+
+
+# import complex math module
+import csv
+# import cmath
+#
+# a = float(input('Enter a: '))
+# b = float(input('Enter b: '))
+# c = float(input('Enter c: '))
+#
+# # calculate the discriminant
+# d = (b ** 2) - (4 * a * c)
+#
+# # find two solutions
+# sol1 = (-b - cmath.sqrt(d)) / (2 * a)
+# sol2 = (-b + cmath.sqrt(d)) / (2 * a)
+# print('The solution are {0} and {1}'.format(sol1, sol2))
+import random
+
+with open('number_for_decorator.csv', 'a', newline='') as csvfile:
+    list_for_csv = [i for i in range(1, 100)]
+    random.shuffle(list_for_csv)
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(list_for_csv)
+
+
+
+with open('number_for_decorator.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
