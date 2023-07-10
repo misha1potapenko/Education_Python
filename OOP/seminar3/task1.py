09383d86db55e07086c72e2705cfe505057b019e
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-10 20:46:19 +0300

    added JSON

--- OOP/seminar3/task1.py
+++ OOP/seminar3/task1.py
@@ -11,10 +11,44 @@ from collections import defaultdict
 class Factorial:
 
     def __init__(self):
-        self.storage = defaultdict(list)
+        self.results = defaultdict(list)
 
-    def __call__(self, value):
-        factorial = 1
-        for i in range(2, value+1):
-            factorial *= i
-        return factorial
+    def __call__(self, number):
+        result = 1
+        for i in range(1, number + 1):
+            result *= i
+        self.results[number].append(result)
+
+    def __str__(self):
+        txt = '\n'.join((f'{k}: {v}' for k, v in self.results.items()))
+        return txt
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        new_file = open("manager_file.json", "w", encoding="utf-8")
+        json.dump(self.results, new_file, indent=4, ensure_ascii=False)
+        new_file.close()
+
+factor = Factorial()
+
+factor(1)
+factor(10)
+factor(3)
+factor(2)
+
+print(factor)
+with factor as new_file:
+    new_file(10)
+
+
+# factor = Factorial()
+#
+# factor(1)
+# factor(10)
+# factor(6)
+#
+# print(factor)
+
+
