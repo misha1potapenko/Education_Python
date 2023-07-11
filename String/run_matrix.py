7e75f60064fa933c06a81ed5398b61292dd39c1c
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-11 18:41:29 +0300

    added print matrix

---
+++ String/run_matrix.py
@@ -0,0 +1,17 @@
+def print_matrix(matrix):
+    string = ''
+    for i in matrix:
+        for j in i:
+            string += '%s\t' % j
+        string = string[:-1]
+        string += '\n'
+    string = string[:-1]
+    return string
+
+matr1 = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+
+matr2 = [[10, 11, 12],
+         [13, 14, 15],
+         [16, 17, 18]]
