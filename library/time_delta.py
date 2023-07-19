cdaae330f15aff6f12c9368b063f9ebe94ed690c
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-19 18:13:11 +0300

    added  library_python iso_date

--- library/time_delta.py
+++ library/time_delta.py
@@ -14,3 +14,44 @@ birthday = datetime(1503, 12, 14)
 dlt = timedelta(days=365.25 * 33)
 new_date = birthday + dlt
 print(f'{new_date = }\t-\t{new_date}')
+
+from datetime import time, date, datetime, timedelta
+d = date(year=2007, month=6, day=15)
+t = time(hour=2, minute=14, microsecond=24)
+dt = datetime(year=2007, month=6, day=15, hour=2, minute=14,
+microsecond=24)
+delta = timedelta(weeks=53, hours=73, minutes=101,
+seconds=303, milliseconds=60)
+print(f'{d.month}')
+print(f'{t.second}')
+print(f'{dt.hour}')
+print(f'{delta.days}')
+
+
+
+t = time(hour=2, minute=14, microsecond=24)
+dt = datetime(year=2007, month=6, day=15, hour=2, minute=14, microsecond=24)
+new_dt = dt.replace(year=2012)
+one_more_hour = t.replace(t.hour + 1)
+print(f'{new_dt}\n{one_more_hour}')
+
+dt = datetime(year=2007, month=6, day=15, hour=2, minute=14, microsecond=24)
+print(dt)
+print(dt.timestamp())
+print(dt.isoformat())
+print(dt.weekday())
+print(dt.strftime('Дата %d %B %Y. День недели %A. Время %H:%M:%S. Это %W неделя и %j день года.'))
+
+from datetime import datetime
+date_original = '2022-12-12 18:01:21.555470'
+date_timestamp = 1181862840.000024
+date_iso = '2007-06-15T02:14:00.000024'
+date_text = 'Дата 15 June 2007. День недели Friday. Время 02:14:00. Это 24 неделя и 166 день года.'
+original_date = datetime.fromisoformat(date_original)
+timestamp_date = datetime.fromtimestamp(date_timestamp)
+iso_date = datetime.fromisoformat(date_iso)
+text_date = datetime.strptime(date_text, 'Дата %d %B %Y. День недели %A. Время %H:%M:%S. Это %W неделя и %j день года.')
+print(original_date)
+print(timestamp_date)
+print(iso_date)
+print(text_date)
