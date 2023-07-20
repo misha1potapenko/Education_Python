84ede1ba80484f2cadb7c3daa4f9e2ac5755d130
misha1potapenko <misha1potapenko@yandex.ru>
2023-07-20 16:27:34 +0300

    added  second for seminar logging

--- library/seminar/first.py
+++ library/seminar/first.py
@@ -4,13 +4,16 @@
 # Например отлавливаем ошибку деления на ноль.
 import logging
 import random
+from datetime import datetime
 
 
 def not_zero():
     number = int(input('Input number = '))
     if number == 0:
-        logging.basicConfig(filename='log.log', filemode='w', encoding='utf-8', level=logging.INFO)
-        logging.error('Input number = 0')
+        logging.basicConfig(filename='log.log', filemode='a', encoding='utf-8', level=logging.INFO)
+        logging.error('Input number = 0' ' ZeroDivisionError: division by zero')
+        current_now = datetime.now()
+        logging.info(current_now)
     result = random.randint(1, 100)/number
     return result
 not_zero()
--- library/seminar/second.py
+++ library/seminar/second.py
@@ -4,3 +4,31 @@
 # работы в файл.
 # Напишите аналогичный декоратор, но внутри используйте
 # модуль logging.
+import logging
+import time
+from typing import Callable
+
+
+def main(func: Callable):
+    def wrapper(*args, **kwargs):
+        logging.basicConfig(filename='decorator_log.log', filemode='a', encoding='utf-8', level=logging.DEBUG)
+        logging.debug(f'Запуск функции {func.__name__} в {time.time()}')
+        print(f'Запуск функции {func.__name__} в {time.time()}')
+        result = func(*args, **kwargs)
+        logging.info(f'Результат функции {func.__name__}: {result}')
+        print(f'Результат функции {func.__name__}: {result}')
+        print(f'Завершение функции {func.__name__} в {time.time()}')
+        return result
+
+    return wrapper
+
+
+@main
+def my_factorial(number: int):
+    if number == 1:
+        return 1
+    else:
+        return number * my_factorial(number - 1)
+
+
+print(f'Factorial = {my_factorial(8)}')
