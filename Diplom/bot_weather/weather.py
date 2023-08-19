b55f2ce6ce4f5e59594af4d3a6b31147d8f08021
misha1potapenko <misha1potapenko@yandex.ru>
2023-08-19 17:53:08 +0300

    added  bot_weather weather

--- Diplom/bot_weather/weather.py
+++ Diplom/bot_weather/weather.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from config import tok
 from pprint import pprint
@@ -15,7 +17,16 @@ def get_weather(tok):
         data = r.json()
         pprint(data)
         show_city = data['city']['name']
-        print(show_city)
+        sunrise = datetime.datetime.fromtimestamp(data['city']['sunrise'])
+        sunset = datetime.datetime.fromtimestamp(data['city']['sunset'])
+        weather = data['list'][0]['dt_txt']
+        for i in range(40):
+            print(data['list'][i]['dt_txt'])
+            print(data['list'][i]['weather'][0]['description'])
+        print(f"Город: {show_city} Восход солнца: {sunrise} Заход солнца: {sunset}\n"
+              f" Прогноз на {weather}")
+
+
     except Exception as ex:
         print(ex)
         print("Что-то пошло не так")
